e226034365ced57871456bfa38868c29354f9dea
Snorre <snle001@student.kristiania.no>
2022-09-14 11:07:00 +0200

    Setup for arbeidskrav_1 project

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    void mainTest() {
+        assertEquals(1, 1);
+    }
+
+
+}
