7e026e5f69ab184f25a692b8d6df9e44d0e8edc7
jalvsaker <jalvsaker@users.noreply.github.com>
2022-09-15 11:12:37 +0200

    🔵 extract code to separate methods

--- src/main/java/no/kristiania/poker/PokerHands.java
+++ src/main/java/no/kristiania/poker/PokerHands.java
@@ -6,12 +6,12 @@ import java.util.Map;
 
 public class PokerHands {
 
-    public String checkWinner(Card[] blackHand, Card[] whiteHand) {
-        HashMap<Value, Integer> blackValueFrequency = new HashMap<>();
-        HashMap<Value, Integer> whiteValueFrequency = new HashMap<>();
+    private final HashMap<Value, Integer> blackValueFrequency = new HashMap<>();
+    private final HashMap<Value, Integer> whiteValueFrequency = new HashMap<>();
+    private final HashMap<Suit, Integer> blackSuitFrequency = new HashMap<>();
+    private final HashMap<Suit, Integer> whiteSuitFrequency = new HashMap<>();
 
-        HashMap<Suit, Integer> blackSuitFrequency = new HashMap<>();
-        HashMap<Suit, Integer> whiteSuitFrequency = new HashMap<>();
+    public String checkWinner(Card[] blackHand, Card[] whiteHand) {
 
         for (Card card : blackHand) {
             blackValueFrequency.merge(card.value, 1, Integer::sum);
@@ -23,126 +23,51 @@ public class PokerHands {
         }
 
 
+        String flushResult = checkFlush();
+        if (flushResult != null) return flushResult;
 
-//      -- Flush --
-        if (blackSuitFrequency.containsValue(5) || whiteSuitFrequency.containsValue(5)){
-            ArrayList<Value> blackValues = new ArrayList<>();
-            ArrayList<Value> whiteValues = new ArrayList<>();
 
-            for (Map.Entry<Value, Integer> freq : blackValueFrequency.entrySet()) {
-                blackValues.add(freq.getKey());
-            }
-            for (Map.Entry<Value, Integer> freq : whiteValueFrequency.entrySet()) {
-                whiteValues.add(freq.getKey());
-            }
+        String straightResult = checkStraight();
+        if (straightResult != null) return straightResult;
 
-            blackValues.sort(null);
-            whiteValues.sort(null);
 
-            if (blackSuitFrequency.containsValue(5) && !whiteSuitFrequency.containsValue(5)){
-                return "Black wins. - with flush: " + blackValues.get(blackValues.size() -1)
-                        + " " + blackSuitFrequency.keySet().toArray()[0];
-            }
-            if (!blackSuitFrequency.containsValue(5) && whiteSuitFrequency.containsValue(5)) {
-                return "White wins. - with flush: " + whiteValues.get(whiteValues.size() -1)
-                        + " " + whiteSuitFrequency.keySet().toArray()[0];
-            }
+        String threeOfAKindResult = checkThreeOfAKind();
+        if (threeOfAKindResult != null) return threeOfAKindResult;
 
-            if (blackValues.get(blackValues.size() -1).compareTo(whiteValues.get(whiteValues.size() -1)) > 0){
-                return "Black wins. - with flush: " + blackValues.get(blackValues.size() -1)
-                        + " " + blackSuitFrequency.keySet().toArray()[0];
-            }
-            if (blackValues.get(blackValues.size() -1).compareTo(whiteValues.get(whiteValues.size() -1)) < 0){
-                return "White wins. - with flush: " + whiteValues.get(whiteValues.size() -1)
-                        + " " + whiteSuitFrequency.keySet().toArray()[0];
-            }
+
+        String pairAndTwoPairsResult = checkPairAndTwoPairs();
+        if (pairAndTwoPairsResult != null) return pairAndTwoPairsResult;
+
+
+        String highCardResult = checkHighCard(blackHand, whiteHand);
+        if (highCardResult != null) return highCardResult;
+
+        return "Tie";
+    }
+
+    private static String checkHighCard(Card[] blackHand, Card[] whiteHand) {
+        //        --- HighCard ---
+        Card blackHigh = new Card(Value.TWO, Suit.SPADES);
+        Card whiteHigh = new Card(Value.TWO, Suit.SPADES);
+
+        for (Card card : blackHand){
+            if (card.value.compareTo(blackHigh.value) > 0) blackHigh.value = card.value;
+        }
+        for (Card card : whiteHand){
+            if (card.value.compareTo(whiteHigh.value) > 0) whiteHigh.value = card.value;
         }
 
-
-//        --- Straight ---
-        int blackStraight = 0;
-        int whiteStraight = 0;
-        Value blackStraightHigh = Value.TWO;
-        Value whiteStraightHigh = Value.TWO;
-
-        /*
-        *   if counter gets to five it is a straight.
-        *   saves the highest value of straight and compare to the other hand
-        * */
-        for (Value value : Value.values()) {
-            if (blackValueFrequency.containsKey(value)) {
-                blackStraight++;
-                if (blackStraight == 5) {
-                    blackStraightHigh =  value;
-                }
-            } else {
-                blackStraight = 0;
-            }
-
-            if (whiteValueFrequency.containsKey(value)) {
-                whiteStraight++;
-                if (whiteStraight == 5) {
-                    whiteStraightHigh =  value;
-                }
-            } else {
-                whiteStraight = 0;
-            }
+        if (blackHigh.value.compareTo(whiteHigh.value) > 0) {
+            return "Black wins. - with high card: " + blackHigh.value;
         }
-
-        if (!blackStraightHigh.equals(Value.TWO) || !whiteStraightHigh.equals(Value.TWO)) {
-            if (blackStraightHigh.compareTo(whiteStraightHigh) > 0) {
-                return "Black wins. - with straight: " + blackStraightHigh;
-            }
-            if (blackStraightHigh.compareTo(whiteStraightHigh) < 0) {
-                return "White wins. - with straight: " + whiteStraightHigh;
-            }
-
-            if (!blackStraightHigh.equals(Value.TWO)) {
-                return "Black wins. - with straight: " + blackStraightHigh;
-            } else {
-                return "White wins. - with straight: " + whiteStraightHigh;
-            }
-
+        if (blackHigh.value.compareTo(whiteHigh.value) < 0){
+            return "White wins. - with high card: " + whiteHigh.value;
         }
+        return null;
+    }
 
-
-
-//        --- Three of a kind ---
-        if (whiteValueFrequency.containsValue(3)||blackValueFrequency.containsValue(3)){
-            Value blackThree = Value.TWO;
-            Value whiteThree = Value.TWO;
-
-            for (Map.Entry<Value, Integer> freq : blackValueFrequency.entrySet()) {
-                if (freq.getValue() == 3 && freq.getKey().compareTo(blackThree) > 0){
-                    blackThree = freq.getKey();
-                }
-            }
-
-            for (Map.Entry<Value, Integer> freq : whiteValueFrequency.entrySet()) {
-                if (freq.getValue() == 3 && freq.getKey().compareTo(whiteThree) > 0){
-                    whiteThree = freq.getKey();
-                }
-            }
-
-            if (blackValueFrequency.containsValue(3) && whiteValueFrequency.containsValue(3)){
-                if (blackThree.compareTo(whiteThree) > 0){
-                    return "Black wins. - with three of a kind card: " + blackThree;
-                }
-                if (whiteThree.compareTo(blackThree) > 0) {
-                    return "White wins. - with three of a kind card: " + whiteThree;
-                }
-            }
-            if(blackValueFrequency.containsValue(3)){
-                return "Black wins. - with three of a kind card: " + blackThree;
-            }
-            if(whiteValueFrequency.containsValue(3)){
-                return "White wins. - with three of a kind card: " + whiteThree;
-            }
-        }
-
-
-
-//        --- Pair ---
+    private String checkPairAndTwoPairs() {
+        //        --- Pair ---
         if (whiteValueFrequency.containsValue(2) || blackValueFrequency.containsValue(2)){
             var blackPairs = new ArrayList<Value>();
             var whitePairs = new ArrayList<Value>();
@@ -207,28 +132,129 @@ public class PokerHands {
                 return "Black wins. - with pair card: " + blackPairs.get(0);
             }
         }
+        return null;
+    }
 
+    private String checkThreeOfAKind() {
+        //        --- Three of a kind ---
+        if (whiteValueFrequency.containsValue(3)|| blackValueFrequency.containsValue(3)){
+            Value blackThree = Value.TWO;
+            Value whiteThree = Value.TWO;
 
+            for (Map.Entry<Value, Integer> freq : blackValueFrequency.entrySet()) {
+                if (freq.getValue() == 3 && freq.getKey().compareTo(blackThree) > 0){
+                    blackThree = freq.getKey();
+                }
+            }
 
-//        --- HighCard ---
-        Card blackHigh = new Card(Value.TWO, Suit.SPADES);
-        Card whiteHigh = new Card(Value.TWO, Suit.SPADES);
+            for (Map.Entry<Value, Integer> freq : whiteValueFrequency.entrySet()) {
+                if (freq.getValue() == 3 && freq.getKey().compareTo(whiteThree) > 0){
+                    whiteThree = freq.getKey();
+                }
+            }
 
-        for (Card card : blackHand){
-            if (card.value.compareTo(blackHigh.value) > 0) blackHigh.value = card.value;
+            if (blackValueFrequency.containsValue(3) && whiteValueFrequency.containsValue(3)){
+                if (blackThree.compareTo(whiteThree) > 0){
+                    return "Black wins. - with three of a kind card: " + blackThree;
+                }
+                if (whiteThree.compareTo(blackThree) > 0) {
+                    return "White wins. - with three of a kind card: " + whiteThree;
+                }
+            }
+            if(blackValueFrequency.containsValue(3)){
+                return "Black wins. - with three of a kind card: " + blackThree;
+            }
+            if(whiteValueFrequency.containsValue(3)){
+                return "White wins. - with three of a kind card: " + whiteThree;
+            }
         }
-        for (Card card : whiteHand){
-            if (card.value.compareTo(whiteHigh.value) > 0) whiteHigh.value = card.value;
+        return null;
+    }
+
+    private String checkStraight() {
+        //        --- Straight ---
+        int blackStraight = 0;
+        int whiteStraight = 0;
+        Value blackStraightHigh = Value.TWO;
+        Value whiteStraightHigh = Value.TWO;
+
+        /*
+        *   if counter gets to five it is a straight.
+        *   saves the highest value of straight and compare to the other hand
+        * */
+        for (Value value : Value.values()) {
+            if (blackValueFrequency.containsKey(value)) {
+                blackStraight++;
+                if (blackStraight == 5) {
+                    blackStraightHigh =  value;
+                }
+            } else {
+                blackStraight = 0;
+            }
+
+            if (whiteValueFrequency.containsKey(value)) {
+                whiteStraight++;
+                if (whiteStraight == 5) {
+                    whiteStraightHigh =  value;
+                }
+            } else {
+                whiteStraight = 0;
+            }
         }
 
-        if (blackHigh.value.compareTo(whiteHigh.value) > 0) {
-            return "Black wins. - with high card: " + blackHigh.value;
-        }
-        if (blackHigh.value.compareTo(whiteHigh.value) < 0){
-            return "White wins. - with high card: " + whiteHigh.value;
-        }
+        if (!blackStraightHigh.equals(Value.TWO) || !whiteStraightHigh.equals(Value.TWO)) {
+            if (blackStraightHigh.compareTo(whiteStraightHigh) > 0) {
+                return "Black wins. - with straight: " + blackStraightHigh;
+            }
+            if (blackStraightHigh.compareTo(whiteStraightHigh) < 0) {
+                return "White wins. - with straight: " + whiteStraightHigh;
+            }
 
-        return "Tie";
+            if (!blackStraightHigh.equals(Value.TWO)) {
+                return "Black wins. - with straight: " + blackStraightHigh;
+            } else {
+                return "White wins. - with straight: " + whiteStraightHigh;
+            }
+
+        }
+        return null;
+    }
+
+    private String checkFlush() {
+        //      -- Flush --
+        if (blackSuitFrequency.containsValue(5) || whiteSuitFrequency.containsValue(5)){
+            ArrayList<Value> blackValues = new ArrayList<>();
+            ArrayList<Value> whiteValues = new ArrayList<>();
+
+            for (Map.Entry<Value, Integer> freq : blackValueFrequency.entrySet()) {
+                blackValues.add(freq.getKey());
+            }
+            for (Map.Entry<Value, Integer> freq : whiteValueFrequency.entrySet()) {
+                whiteValues.add(freq.getKey());
+            }
+
+            blackValues.sort(null);
+            whiteValues.sort(null);
+
+            if (blackSuitFrequency.containsValue(5) && !whiteSuitFrequency.containsValue(5)){
+                return "Black wins. - with flush: " + blackValues.get(blackValues.size() - 1)
+                        + " " + blackSuitFrequency.keySet().toArray()[0];
+            }
+            if (!blackSuitFrequency.containsValue(5) && whiteSuitFrequency.containsValue(5)) {
+                return "White wins. - with flush: " + whiteValues.get(whiteValues.size() - 1)
+                        + " " + whiteSuitFrequency.keySet().toArray()[0];
+            }
+
+            if (blackValues.get(blackValues.size() -1).compareTo(whiteValues.get(whiteValues.size() -1)) > 0){
+                return "Black wins. - with flush: " + blackValues.get(blackValues.size() - 1)
+                        + " " + blackSuitFrequency.keySet().toArray()[0];
+            }
+            if (blackValues.get(blackValues.size() -1).compareTo(whiteValues.get(whiteValues.size() -1)) < 0){
+                return "White wins. - with flush: " + whiteValues.get(whiteValues.size() - 1)
+                        + " " + whiteSuitFrequency.keySet().toArray()[0];
+            }
+        }
+        return null;
     }
 
 }
